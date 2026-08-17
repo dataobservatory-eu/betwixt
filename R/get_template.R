c1ab2f9f1276f8ab08121c57b08286aa00157152
Daniel Antal <antaldaniel@gmail.com>
2026-08-17 10:37:01 +0200

    get_template()

--- R/get_template.R
+++ R/get_template.R
@@ -10,25 +10,71 @@
 #'
 #' @keywords internal
 #' @noRd
-get_template <- function(
-    template = "default"
-) {
+get_template <- function(template = "default") {
+  legacy_templates <- c(
+    default = "templates/default.mustache",
+    image_review = "templates/image_review.mustache"
+  )
 
-  template_file <- system.file(
-    "templates",
-    paste0(template, ".mustache"),
+  review_templates <- list(
+    long_review = c(
+      html = "long_review/template_long.mustache",
+      css = "long_review/long_style.css",
+      js = "long_review/long_script.js"
+    ),
+    wide_review = c(
+      html = "wide_review/template_wide.mustache",
+      css = "wide_review/wide_style.css",
+      js = "wide_review/wide_script.js"
+    )
+  )
+
+  if (template %in% names(legacy_templates)) {
+    path <- system.file(
+      legacy_templates[[template]],
+      package = "betwixt"
+    )
+
+    if (!nzchar(path)) {
+      stop("Template file not found: '", template, "'.", call. = FALSE)
+    }
+
+    return(
+      paste(
+        readLines(path, warn = FALSE, encoding = "UTF-8"),
+        collapse = "\n"
+      )
+    )
+  }
+
+  if (!template %in% names(review_templates)) {
+    stop("Unknown template: '", template, "'.", call. = FALSE)
+  }
+
+  files <- review_templates[[template]]
+
+  paths <- vapply(
+    files,
+    system.file,
+    FUN.VALUE = character(1),
     package = "betwixt"
   )
 
-  if (!nzchar(template_file)) {
+  missing <- names(paths)[!nzchar(paths)]
+
+  if (length(missing)) {
     stop(
-      "Unknown template: '", template, "'.",
+      "Missing ", template, " template file(s): ",
+      paste(missing, collapse = ", "),
+      ".",
       call. = FALSE
     )
   }
 
-  paste(
-    readLines(template_file, warn = FALSE),
-    collapse = "\n"
-  )
+  lapply(paths, function(path) {
+    paste(
+      readLines(path, warn = FALSE, encoding = "UTF-8"),
+      collapse = "\n"
+    )
+  })
 }

4f4d027b85be36a4a12070dc55ebdac8994bf3d5
Daniel Antal <antaldaniel@gmail.com>
2026-09-01 15:52:46 +0200

    remove unnecessary test

--- tests/testthat/test-review_context_html.R
+++ tests/testthat/test-review_context_html.R
@@ -1,11 +1,6 @@
 test_that("subject definitions identify resolved entities", {
   context <- prepare_review_context(betwixt::delini)
 
-  expect_equal(
-    context$rows[[1]]$assertions[[1]]$definition,
-    "https://reprexbase.eu/fu/Item:Q7328"
-  )
-
   expect_true(is.na(
     context$rows[[2]]$assertions[[1]]$definition
   ))

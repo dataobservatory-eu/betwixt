2176113c915023fc9e5fe344ac25a07433c6823f
Daniel Antal <antaldaniel@gmail.com>
2026-09-13 13:41:56 +0200

    update utils

--- R/utils.R
+++ R/utils.R
@@ -3,3 +3,26 @@
 betwixt_version <- function() {
   as.character(getNamespaceInfo(asNamespace("betwixt"), "spec")["version"])
 }
+
+
+#' @noRd
+#' @keywords internal
+escape_html <- function(x) {
+  x <- as.character(x)
+  x <- gsub("&", "&amp;", x, fixed = TRUE)
+  x <- gsub("<", "&lt;", x, fixed = TRUE)
+  x <- gsub(">", "&gt;", x, fixed = TRUE)
+  x <- gsub('"', "&quot;", x, fixed = TRUE)
+  x
+}
+
+#' @noRd
+#' @keywords internal
+qualification_html <- function() {
+  paste0(
+    '<div class="qualify">',
+    '<button data-qualify="defer">Defer</button>',
+    '<button data-qualify="reject">Reject</button>',
+    "</div>"
+  )
+}
--- tests/testthat/test-utils.R
+++ tests/testthat/test-utils.R
@@ -2,3 +2,22 @@ test_that("betwixt version works", {
   expect_true(is.character(betwixt_version()))
   expect_true(grepl(pattern = "0.0", x = betwixt_version()))
 })
+
+test_that("escape_html escapes HTML special characters", {
+  expect_equal(
+    escape_html('A & B <tag> "quoted"'),
+    "A &amp; B &lt;tag&gt; &quot;quoted&quot;"
+  )
+})
+
+test_that("escape_html coerces values to character", {
+  expect_equal(escape_html(123), "123")
+})
+
+test_that("qualification_html renders qualification controls", {
+  html <- qualification_html()
+
+  expect_match(html, 'class="qualify"', fixed = TRUE)
+  expect_match(html, 'data-qualify="defer"', fixed = TRUE)
+  expect_match(html, 'data-qualify="reject"', fixed = TRUE)
+})
